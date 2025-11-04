4d1d602f52f1930d5826a81da62a700c6a5d2a63
Lance Fernandez <lfernande3-c@my.cityu.edu.hk>
2025-11-04 22:49:02 +0800

    Remove legacy Kotlin compiler session file and pages barrel export from the presentation layer.

--- lib/legacy/presentation/pages/pages.dart
+++
@@ -1,8 +0,0 @@
-// Pages barrel export
-export 'home_page.dart';
-export 'onboarding_page.dart';
-export 'timetable_page.dart';
-export 'qr_page.dart';
-export 'settings_page.dart';
-export 'login_page.dart';
-export 'booking_page.dart';
